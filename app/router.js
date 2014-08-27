ac5a8226e5737047d168667f7c0be7257b644c91
Nico Gevers <nico@squire-labs.com>
2014-08-27 10:43:12 +0200

    Updated ember-cli to 0.0.41

--- app/router.js
+++ app/router.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 var Router = Ember.Router.extend({
-  location: EmberFiltertableENV.locationType
+  location: EmberCliFiltertableENV.locationType
 });
 
 Router.map(function() {
