27f19c33933b27ed8aa5d6c13545c466a72a7aa2
Joshua Cope <jcope2010@gmail.com>
2014-10-15 19:46:38 -0400

    fix module export to use ember-cli-filtertable-inbox-health

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  name: 'ember-cli-filtertable',
+  name: 'ember-cli-filtertable-inbox-health',
   included: function(app) {
     this._super.included(app);
     app.import('vendor/filtertable.css');
